12c02ce10cf27b6158cadf982425969640e6b2a9
Niaz <tigrexspalterlp@gmail.com>
2021-06-19 12:50:30 +0200

    Interval, IntervalMap and insert works now

--- uebung3.hs
+++ uebung3.hs
@@ -1,22 +1,21 @@
--- Also grundsätzlich wird ein Wert für ein Intervall statt einem Key implementiert
--- Ergo brauchen wir erstmal einen Datentyp Intervall
-data Intervall = Intervall [Integer]
--- Ein paar besteht immer aus eine Intervall und einem Wert
--- Wir haben aber natürlich nicht nur ein Intervall und einen Value, sondern beliebig viele
-data IntervallMap k v = IntervallMap Intervall k v 
+-- Wir brauchen erstmal einen Datentyp Intervall
+data Intervall startPoint endPoint = Intervall startPoint endPoint
 
--- singleton erstellt eine Intervall Map mit einem Intervall von 0 - unendlich
-singleton :: v -> IntervallMap k v 
-singleton v = IntervallMap [0..] v
+-- Ein IntervalMap besteht aus vielen Intervall-Wert Paaren oder es ist ein Singleton
+data IntervalMap k v = IntervalMap [(Intervall k k, v)] v | Singleton v
 
--- ord steht für Ordered. Wir machen das natürlich anhand von dem Wert k aka Intervall
--- Die Idee ist, dass wir das k in die beiden Werte zurückbringen und dann vergleichen,
--- ob der erste Wert von Wert a größer ist als der letzte Wert von b.
--- (!) :: Ord k => IntervalMap k v -> k -> v
- 
+-- Hier initialsieren wir dann einfach unser Singleton
+singleton :: v -> IntervalMap k v 
+singleton v = Singleton v
+
+-- Ord erstmal nur für Singleton / IntervalMap fehlt noch
+(!) :: Ord k => IntervalMap k v -> k -> v
+(!) (Singleton v) x = v
+
 -- da Datentypen nicht veränderbar sind, muss hier eine neue IntervallMap zurückgegeben werden
--- insert :: Ord k => k -> k -> v -> IntervalMap k v -> IntervalMap k v
--- insert x, y, v = IntervallMap (x, y) v
+insert :: Ord k => k -> k -> v -> IntervalMap k v -> IntervalMap k v
+insert x y v (Singleton defaultValue) = IntervalMap [((Intervall x y), v)] defaultValue
+insert x y v (IntervalMap xs defaultValue) = IntervalMap (xs ++ [((Intervall x y), v)]) defaultValue
 
 -- das wird benutzt, damit wir funktionen anwenden können auf eine IntervalMap
 -- instance Functor (IntervalMap k) where
