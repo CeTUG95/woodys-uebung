5e81f709210b69097f776d851c4c50c070365c8a
Niaz <tigrexspalterlp@gmail.com>
2021-06-19 18:01:54 +0200

    Funktor funktioniert teilweise, aber es fehlt ein Case

--- uebung3.hs
+++ uebung3.hs
@@ -15,20 +15,25 @@ singleton v = Singleton v
 (!) (IntervalMap ((Intervall a b, v) : xs) defaultValue) inputValue | inputValue >= a && inputValue <= b = v 
 																    | otherwise = (!) (IntervalMap xs defaultValue) inputValue
 
--- To-Do: Stelle sicher, dass es nur für einen Intervall auch nur ein Intervall-Wert Paar gibt
 insert :: Ord k => k -> k -> v -> IntervalMap k v -> IntervalMap k v
 insert x y v (Singleton defaultValue) = IntervalMap [((Intervall x y), v)] defaultValue
 insert x y v (IntervalMap xs defaultValue) = IntervalMap ((remove x y xs) ++ [((Intervall x y), v)]) defaultValue
 
+-- To-Do: Ist off bei einem Wert
 remove :: Ord k => k -> k -> [(Intervall k k, v)] -> [(Intervall k k, v)]
 remove x y [] = []
-remove x y ((Intervall a b, v):xs) | x < a && y > b = remove x y xs 						-- liegt innerhalb der range und wird entfernt					
-                                   | a < x && y > b = remove x y xs ++ [(Intervall a x, v)] -- der anfang liegt nicht innerhalb in der range
-								   | x < a && b > y = remove x y xs ++ [(Intervall y b, v)] -- das ende liegt nicht innerhalb in der range
-								   | a < x && b > y = remove x y xs ++ [(Intervall a x, v)] ++ [(Intervall y b, v)] -- der anfang und das ende liegt nicht innerhalb in der range
+remove x y ((Intervall a b, v):xs) | x <= a && y >= b = remove x y xs 						-- liegt innerhalb der range und wird entfernt					
+                                   | a <= x && y >= b = remove x y xs ++ [(Intervall a x, v)] -- der anfang liegt nicht innerhalb in der range
+								   | x <= a && b >= y = remove x y xs ++ [(Intervall y b, v)] -- das ende liegt nicht innerhalb in der range
+								   | a <= x && b >= y = remove x y xs ++ [(Intervall a x, v)] ++ [(Intervall y b, v)] -- der anfang und das ende liegt nicht innerhalb in der range
 								   
 -- das wird benutzt, damit wir funktionen anwenden können auf eine IntervalMap
--- instance Functor (IntervalMap k) where
-
+-- Hier gibt es einen Case, der fehlt...
+instance Functor (IntervalMap k) where
+	fmap f (Singleton v) = Singleton (f v)
+	fmap f (IntervalMap [((Intervall a b), v)] defaultValue) = IntervalMap [((Intervall a b), (f v))] (f defaultValue)
+	
 -- das wird benutzt um Operationen aneinanderzuhängen
 -- instance Ord k => Applicative (IntervalMap k) where
+-- 	pure f = f k 
+--	(IntervalMap k) f g <*> = IntervalMap [((Intervall a b), (f g v))] (f g defaultValue)
