fdb1b904522967a4cebbe35d4454fb66117ed80f
bdruesedow <b.druesedow@googlemail.com>
2021-06-24 15:26:09 +0200

    add user interaction, add funktion 1 (add Address)

--- aufgabe4.hs
+++ aufgabe4.hs
@@ -1,41 +1,103 @@
+module Main where
+import System.IO
+
+-- deklaration des files
+file = "addresses.txt"
+
 -- Adresse mit Vorname, Name, Straße, Hausnummer, Postleitzahl, Stadt und Telefonnummer
-data Address = Address
+data Adresse = Adresse
     {
-        firstname :: String,
-        lastname :: String,
-        street :: String,
-        housenumber :: String,
-        zipcode :: String,
-        town :: String,
-        telephone :: String
+        vorname :: String,
+        nachname :: String,
+        strasse :: String,
+        hausnummer :: String,
+        plz :: String,
+        stadt :: String,
+        telefon :: String
     } deriving (Ord, Eq, Show)
 
-getAddress :: Address -> String
-getAddress (Address firstName lastName street housenumber zipcode town telephone) = firstName ++ "," ++ lastName ++ "," ++ street ++ "," ++ housenumber ++ "," ++ zipcode ++ "," ++ town ++ "," ++ telephone ++ "\n"
+getAdresse :: Adresse -> String
+getAdresse (Adresse vorname nachname strasse hausnummer plz stadt telefon) = vorname ++ ";" ++ nachname ++ ";" ++ strasse ++ ";" ++ hausnummer ++ ";" ++ plz ++ ";" ++ stadt ++ ";" ++ telefon ++ "\n"
 
--- readAddress soll aus einem Komma-seperierten String eine Addresse machen
--- wir brauchen eine Methode um den String bei den Kommas aufzutrennen und anschließend in den Konstruktor für Address zu geben
--- zb: String zu List of String zu Address?
---readAddress :: String -> Address
---readAddress addrString =
-
-
-
+main :: IO()
 main = do
-    -- Datei festlegen
-    let file = "addresses.txt"
+    hSetBuffering stdout NoBuffering
+    putStrLn "\n\n===================================="
+    putStrLn "Waehle einer der folgenden Aktionen:"
+    putStrLn "1. Kontakt hinzufuegen"
+    putStrLn "2. Kontakt loeschen"
+    putStrLn "3. Kontakt editieren"
+    putStrLn "4. Alle Kontakte anzeigen"
+    putStrLn "5. Kontakt mit Vornamen suchen"
+    putStrLn "6. Kontakt mit Nachnamen suchen"
+    putStrLn "7. Beenden"
+    putStrLn "------------------------------------"
+    putStr "Eingabe: "
+    input <- readLn
+    putStrLn "====================================\n"
+    case input of
+        1 -> do
+            -- Erfassen der Eingabedaten
+            putStrLn "Kontakt hinzfuegen:"
+            putStr "    Vorname: "
+            vorname <- getLine
+            putStr "    Nachname: "
+            nachname <- getLine
+            putStr "    Strasse: "
+            strasse <- getLine
+            putStr "    Hausnummer: "
+            hausnummer <- getLine
+            putStr "    Postleitzahl: "
+            plz <- getLine
+            putStr "    Stadt: "
+            stadt <- getLine
+            putStr "    Telefonnummer: "
+            telefon <- getLine
+            -- neue Adresse erstellen
+            let adresseNeu = Adresse vorname nachname strasse hausnummer plz stadt telefon
+            appendFile file (getAdresse adresseNeu)
+            putStrLn "\nAdresse gespeichert.\n"
+            main
 
-    -- Dateiinhalt lesen und in variable Speichern
-    addresses <- readFile file -- ohne zwischenschritt zwischen Lesen und schreiben, ist die Datei noch "locked"
-    putStrLn ("Alte Datei:\n" ++ addresses)
+        2 -> do
+            putStrLn "Kontakt loeschen:"
+            main
 
-    -- neue Addresse deklarieren und in die Datei schreiben
-    let addr = Address "Testo" "Testodor" "Testweg" "999" "54321" "Testdorf" "0198765432"
-    writeFile file (addresses ++ getAddress addr)
+        3 -> do
+            putStrLn "Kontakt editieren:"
+            main
 
-    -- erneut auslesen und printen
-    newAddresses <- readFile file
-    putStrLn ("Neue Datei:\n" ++ newAddresses)
+        4 -> do
+            putStrLn "Alle Kontakte:"
+            main
+
+        5 -> do
+            putStrLn "Kontakt mit Vornamen suchen:"
+            main
+
+        6 -> do
+            putStrLn "Kontakt mit Nachnamen suchen:"
+            main
+
+        7 -> do
+            putStrLn "Auf Wiedersehen!"
+
+        _ -> do
+            putStrLn "Fehler: Ungueltige Eingabe! Bitte waehle eine Zahl von 1 bis 7."
+            main
+
+
+    -- -- Dateiinhalt lesen und in variable Speichern
+    -- addresses <- readFile file -- ohne zwischenschritt zwischen Lesen und schreiben, ist die Datei noch "locked"
+    -- putStrLn ("Alte Datei:\n" ++ addresses)
+    --
+    -- -- neue Addresse deklarieren und in die Datei schreiben
+    -- let addr = Address "Testo" "Testodor" "Testweg" "999" "54321" "Testdorf" "0198765432"
+    -- writeFile file (addresses ++ getAddress addr)
+    --
+    -- -- erneut auslesen und printen
+    -- newAddresses <- readFile file
+    -- putStrLn ("Neue Datei:\n" ++ newAddresses)
 
 
 
